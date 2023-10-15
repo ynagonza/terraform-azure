72daa15849fff11a67cb220b0a100d69f3084780
smazzochi <75561015+smazzochi@users.noreply.github.com>
2023-10-15 19:45:36 +0100

    Adding Public IP

--- main.tf
+++ main.tf
@@ -70,5 +70,15 @@ resource "azurerm_subnet_network_security_group_association" "mtc-sga" {
   network_security_group_id = azurerm_network_security_group.mtc-sg.id
 }
 
+resource "azurerm_public_ip" "mtc-ip" {
+  name                = "mtc-ip"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "dev"
+  }
+}
 
 
