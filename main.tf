4b0850d0a5812182050a715aea13562496537594
ynagonza <147992976+ynagonza@users.noreply.github.com>
2023-10-15 21:09:59 +0100

    changed destination port range

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "azurerm_network_security_rule" "mtc-dev-rule" {
   access                      = "Allow"
   protocol                    = "*"
   source_port_range           = "*"
-  destination_port_range      = "*"
+  destination_port_range      = "443"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.mtc-rg.name
